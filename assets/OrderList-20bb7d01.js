a3370b9c3c93a87116fa6735ae13ed731f34caf9
Kevin <a039386773@gmial.com>
2023-03-02 00:28:39 +0800

    deploy

--- assets/OrderList-20bb7d01.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./index-ecb93a64.js";const r={};function c(s,n,a,_,p,f){return t(),o("h2",null,"這是後台訂單")}const d=e(r,[["render",c]]);export{d as default};
--- assets/ProductList-c5bc8cc9.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as o}from"./index-ecb93a64.js";const c={};function r(s,n,a,_,p,f){return e(),o("h2",null,"這是後端購物車列表")}const u=t(c,[["render",r]]);export{u as default};
